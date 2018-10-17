88514fe622b47c99195d1cc84d462848618b2e65
gv4383 <gregory.vargas18@gmail.com>
2018-10-17 09:19:24 -0500

    Added more information in description of FindAndDelete_Brute algorithm
    Added section talking about the time complexity of the algorithm.

--- javascript/algorithms/FindAndDelete/FindAndDelete_Brute.js
+++ javascript/algorithms/FindAndDelete/FindAndDelete_Brute.js
@@ -12,6 +12,13 @@
 * [0, 1, 2, 3, 0, 4, 2], val = 2
 * returns length = 5
 * The new array contains 0, 1, 2, 3, 0, 4
+*
+* It's important to note the time complexity of this approach.
+* At first glance, it may seem like a time complexity of O(n) because of the for-loop in line 28.
+* If we take the splice method on line 32 into account, it's finding an element at a specific index in an array,
+* deleting it, and then shifting the rest of the elements that come after back one index to fill in the removed element
+*
+* Taking the splice method into account, the time complexity becomes O(n^2) in the worst case scenario.
 */
 
 function findAndDelete(arr, val) {
